9745a090e6aa02a19fffd3049b84de589c768ca8
Abigail Walthall <ghthor@gmail.com>
2014-01-25 07:34:56 -0500

    Simplified the spec setup and expectations

--- watch_cmd_test.go
+++ watch_cmd_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"github.com/ghthor/gospec"
 	. "github.com/ghthor/gospec"
+	"io/ioutil"
 	"os"
 	"path"
 	"time"
@@ -20,15 +21,10 @@ func DescribeWatchCmd(c gospec.Context) {
 
 	c.Specify("the `watch` command", func() {
 		c.Specify("will append the time the entry was completed", func() {
+			fileData := "Some data that shouldn't be modified"
 			testFilename := path.Join(jd, "test_file")
-			f, err := os.OpenFile(testFilename, os.O_CREATE|os.O_RDWR, 0600)
-			c.Assume(err, IsNil)
 
-			fbuf := bufio.NewWriter(f)
-			_, err = fbuf.WriteString("Some data that shouldn't be modified\n")
-			c.Assume(err, IsNil)
-			c.Assume(fbuf.Flush(), IsNil)
-			c.Assume(f.Sync(), IsNil)
+			c.Assume(ioutil.WriteFile(testFilename, []byte(fileData+"\n"), 0600), IsNil)
 
 			statusCh := make(chan string)
 			timeCh := make(chan time.Time)
@@ -44,28 +40,16 @@ func DescribeWatchCmd(c gospec.Context) {
 			c.Expect(<-statusCh, Equals, "watching")
 
 			// Trigger watchEntry via inotify watcher
+			f, err := os.Open(testFilename)
+			c.Assume(err, IsNil)
 			c.Assume(f.Close(), IsNil)
 
 			// Wait for watchEntry to complete it's task
 			completedTime := <-timeCh
 
-			f, err = os.OpenFile(testFilename, os.O_RDONLY, 0600)
+			actualData, err := ioutil.ReadFile(testFilename)
 			c.Assume(err, IsNil)
-			defer f.Close()
-
-			fscan := bufio.NewScanner(f)
-
-			// Check the data is intact
-			c.Expect(fscan.Scan(), IsTrue)
-			c.Expect(fscan.Text(), Equals, "Some data that shouldn't be modified")
-
-			// Check that a newline spacer was put in place
-			c.Expect(fscan.Scan(), IsTrue)
-
-			// Check that the last line in the file is the time the watch completed at
-			c.Expect(fscan.Scan(), IsTrue)
-			c.Expect(fscan.Text(), Equals, completedTime.Format(time.UnixDate))
-			c.Expect(fscan.Scan(), IsFalse)
+			c.Expect(string(actualData), Equals, fmt.Sprintf("%s\n\n%s\n", fileData, completedTime.Format(time.UnixDate)))
 		})
 	})
 }
