e0bca8d7d4cc141119669f81cedccaef5e256a00
Abigail Walthall <ghthor@gmail.com>
2014-01-26 21:03:20 -0500

    Implemented a simple function to find the last entry

---
+++ last_cmd.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func lastEntryFilename(journalDirectory string) (filename string, err error) {
+	var last time.Time = time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	err = filepath.Walk(journalDirectory, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && strings.Index(path, ".git") == -1 {
+			t, err := time.Parse(filenameLayout, filepath.Base(path))
+			if err == nil {
+				if t.After(last) {
+					last = t
+					filename = filepath.Base(path)
+				}
+			}
+		}
+		return nil
+	})
+
+	return
+}
