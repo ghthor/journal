b59817e6e259ca40733920849720f5f72504ce5e
Abigail Walthall <ghthor@gmail.com>
2014-01-28 02:18:46 -0500

    Moved all cmd testing configuration into the _test dir | It's also all going to be generated during the test

--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 var BuildExecutableOnce sync.Once
+var MkTestDirectory sync.Once
 
 func DescribeJournalCommand(c gospec.Context) {
 	BuildExecutableOnce.Do(func() {
@@ -24,13 +25,23 @@ func DescribeJournalCommand(c gospec.Context) {
 		}
 	})
 
+	MkTestDirectory.Do(func() {
+		if err := os.Mkdir("_test", 0755); err != nil {
+			if !os.IsExist(err) {
+				// Only fatal if is isn't that the directory already exists
+				log.Fatal(err)
+			}
+		}
+	})
+
 	c.Specify("the journal command", func() {
 		c.Specify("will create a directory intialize an empty go repository", func() {
-			c.Assume(os.RemoveAll("_test/journal"), IsNil)
+			td := "_test/journal-init"
+			c.Assume(ioutil.WriteFile("_test/config.journal-init.json", []byte(`{"directory":"`+td+`"}`), 0666), IsNil)
 
-			jc := exec.Command("./journal", "-edit=false", "-init", "-config=config.test.json", "new")
+			jc := exec.Command("./journal", "-edit=false", "-init", "-config=_test/config.journal-init.json", "new")
 			c.Expect(jc.Run(), IsNil)
-			c.Expect("_test/journal", IsAGitRepository)
+			c.Expect(td, IsAGitRepository)
 		})
 	})
 }
