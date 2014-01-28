dcd9a8efb6d04f4eeb3c9d8b08ed0db83c2fadab
Abigail Walthall <ghthor@gmail.com>
2014-01-28 00:54:28 -0500

    Specified the Environment variable expansion

--- config/config.go
+++ config/config.go
@@ -3,12 +3,20 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 )
 
 type Config struct {
 	Directory string `json:"directory"`
 }
 
+func (c Config) ExpandEnv() Config {
+	c.Directory = os.ExpandEnv(c.Directory)
+	return c
+}
+
+// Will read the configuration from a .json file
+// The directory will be expanded for Environment variables
 func ReadFromFile(file string) (c Config, err error) {
 	bytes, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -16,5 +24,10 @@ func ReadFromFile(file string) (c Config, err error) {
 	}
 
 	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return
+	}
+
+	c = c.ExpandEnv()
 	return
 }
--- config/config_test.go
+++ config/config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/ghthor/gospec"
 	. "github.com/ghthor/gospec"
+	"os"
 	"testing"
 )
 
@@ -15,13 +16,28 @@ func TestSpecs(t *testing.T) {
 }
 
 func DescribeConfigLoading(c gospec.Context) {
-	c.Specify("Config can be parsed from json file", func() {
-		expectedConfig := Config{
-			"a/path/to/a/git/directory",
-		}
+	c.Specify("a config", func() {
+		c.Specify("can be stored in a json file", func() {
+			expectedConfig := Config{
+				"a/path/to/a/git/directory",
+			}
 
-		config, err := ReadFromFile("config.example.json")
-		c.Assume(err, IsNil)
-		c.Expect(config, Equals, expectedConfig)
+			config, err := ReadFromFile("config.example.json")
+			c.Assume(err, IsNil)
+			c.Expect(config, Equals, expectedConfig)
+		})
+
+		c.Specify("stores a directory", func() {
+			c.Specify("that will have it's environment varibles expanded", func() {
+				expectedConfig := Config{
+					"expanded_path_to/_test",
+				}
+				c.Assume(os.Setenv("A_ENV_VAR", "expanded_path_to"), IsNil)
+
+				config, err := ReadFromFile("config.env_test.json")
+				c.Assume(err, IsNil)
+				c.Expect(config, Equals, expectedConfig)
+			})
+		})
 	})
 }
