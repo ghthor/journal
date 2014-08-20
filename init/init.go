2413c023e0c12ccc88a09096121cf364c0b75333
Willa Walthall <ghthor@gmail.com>
2014-08-20 11:17:03 -0400

    Spec and Impl for CanBeInitialized

--- init/init.go
+++ init/init.go
@@ -2,15 +2,49 @@ package init
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func CanBeInitialized(directory string) bool {
-	_, err := os.Stat(directory)
+	di, err := os.Stat(directory)
+
+	// If the directory doesn't exist
 	if err != nil && os.IsNotExist(err) {
 		return true
 	}
 
-	return false
+	// If the directory exists and is empty
+	if !di.IsDir() {
+		return false
+	}
+
+	numChildren := 0
+	filepath.Walk(directory, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip the base directory
+		if fi.Name() == di.Name() {
+			return nil
+		}
+
+		// Skip a .git repository
+		if strings.Contains(path, ".git") {
+			return nil
+		}
+
+		numChildren++
+
+		return nil
+	})
+
+	if numChildren > 0 {
+		return false
+	}
+
+	return true
 }
 
 func HasBeenInitialized(directory string) bool {
--- init/init_test.go
+++ init/init_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ghthor/gospec"
 	. "github.com/ghthor/gospec"
 	"github.com/ghthor/journal/entry"
+	"github.com/ghthor/journal/git"
 	"github.com/ghthor/journal/git/gittest"
 	"github.com/ghthor/journal/idea"
 	jinit "github.com/ghthor/journal/init"
@@ -109,13 +110,44 @@ func DescribeInit(c gospec.Context) {
 			})
 		})
 
+		tmpDir := func() (directory string, cleanUp func()) {
+			directory, err := ioutil.TempDir("", "journal_init_")
+			c.Assume(err, IsNil)
+
+			cleanUp = func() {
+				c.Assume(os.RemoveAll(directory), IsNil)
+			}
+			return
+		}
+
 		c.Specify("that can be initialized", func() {
 			c.Specify("is an empty directory", func() {
+				directory, cleanUp := tmpDir()
+				defer cleanUp()
+
+				c.Assume(directory, CanBeInitialized)
+
 				c.Specify("inside a git repository", func() {
+					jd := filepath.Join(directory, "journal")
+
+					c.Assume(os.MkdirAll(jd, 0755), IsNil)
+					c.Assume(git.Init(directory), IsNil)
+
+					c.Expect(jd, CanBeInitialized)
 				})
 
 				c.Specify("NOT inside a git repository", func() {
+					c.Expect(directory, CanBeInitialized)
 				})
+			})
+
+			c.Specify("is an empty git repository", func() {
+				directory, cleanUp := tmpDir()
+				defer cleanUp()
+
+				c.Assume(git.Init(directory), IsNil)
+
+				c.Expect(directory, CanBeInitialized)
 			})
 
 			c.Specify("is a directory that doesn't exist", func() {
@@ -129,12 +161,35 @@ func DescribeInit(c gospec.Context) {
 		})
 
 		c.Specify("that can NOT be initialized", func() {
+			directory, cleanUp := tmpDir()
+			defer cleanUp()
+
+			c.Specify("is a file", func() {
+				f, err := ioutil.TempFile(directory, "notadirectory")
+				c.Assume(err, IsNil)
+
+				c.Expect(f.Name(), Not(CanBeInitialized))
+			})
+
 			c.Specify("is a directory", func() {
 				c.Specify("that already contains", func() {
 					c.Specify("an entry directory", func() {
+						c.Expect(directory, CanBeInitialized)
+
+						entryDir := filepath.Join(directory, "entry")
+						c.Assume(os.MkdirAll(entryDir, 0755), IsNil)
+
+						c.Expect(directory, Not(CanBeInitialized))
 					})
 
 					c.Specify("an idea directory store", func() {
+						ideaDir := filepath.Join(directory, "idea")
+						c.Assume(os.Mkdir(ideaDir, 0755), IsNil)
+
+						_, _, err := idea.InitDirectoryStore(ideaDir)
+						c.Assume(err, IsNil)
+
+						c.Expect(directory, Not(CanBeInitialized))
 					})
 				})
 			})
