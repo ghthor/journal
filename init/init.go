b367ce26d36361f69c4738aea659b67b047a59f2
Willa Walthall <ghthor@gmail.com>
2014-08-31 18:43:01 -0400

    Implementation of HasBeenInitialized and init.Journal

--- init/init.go
+++ init/init.go
@@ -4,7 +4,21 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/ghthor/journal/git"
+	"github.com/ghthor/journal/idea"
 )
+
+func isAGitRepository(directory string) bool {
+	c := git.Command(directory, "status", "-s")
+
+	_, err := c.CombinedOutput()
+	if err != nil {
+		return false
+	}
+
+	return true
+}
 
 func CanBeInitialized(directory string) bool {
 	di, err := os.Stat(directory)
@@ -47,10 +61,59 @@ func CanBeInitialized(directory string) bool {
 	return true
 }
 
+func entryStoreIsWithin(directory string) bool {
+	fi, err := os.Stat(filepath.Join(directory, "entry"))
+	if err != nil {
+		return false
+	}
+
+	return fi.IsDir()
+}
+
+func ideaStoreIsWithin(directory string) bool {
+	// TODO Implement a clear function to test for an idea.DirectoryStore existence
+	_, err := idea.NewDirectoryStore(filepath.Join(directory, "idea"))
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
 func HasBeenInitialized(directory string) bool {
-	return false
+	isGit := isAGitRepository(directory)
+	containsEntryStore := entryStoreIsWithin(directory)
+	containsIdeaStore := ideaStoreIsWithin(directory)
+
+	return isGit && containsEntryStore && containsIdeaStore
 }
 
 func Journal(directory string) error {
+	// Check if we need to `git init` the directory
+	if !isAGitRepository(directory) {
+		err := git.Init(directory)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create the Entry Store
+	err := os.Mkdir(filepath.Join(directory, "entry"), 0755)
+	if err != nil {
+		return err
+	}
+
+	// Create the Idea Store
+	err = os.Mkdir(filepath.Join(directory, "idea"), 0755)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = idea.InitDirectoryStore(filepath.Join(directory, "idea"))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
