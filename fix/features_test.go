799a20289faa86337b93974d5a3c006afa886d1f
Willa Walthall <ghthor@gmail.com>
2015-01-07 12:23:59 -0500

    Pseudo code of the fixed journal spec

--- fix/features_test.go
+++ fix/features_test.go
@@ -6,15 +6,24 @@ import (
 
 func DescribeAFixableJournal(c gospec.Context) {
 	c.Specify("a fixed journal is a", func() {
+		// d, _, _ := newCaseCurrent("case_current_spec")
 		c.Specify("directory", func() {
+			// c.Expect(d, isa, directory)
 			c.Specify("inside a git repository", func() {
+				// c.Expect(d, isa, gitrepo)
 			})
 
 			c.Specify("containing", func() {
 				c.Specify("an entry directory", func() {
+					// c.Expect(d, contains, "entry/")
+					c.Specify("with all entries using the current entry format", func() {
+						// c.Expect(d.entries, areCurrentFormat)
+					})
 				})
 
 				c.Specify("an idea directory store", func() {
+					// c.Expect(d.ideas, exists)
+					// c.Expect(d.ideas, isEdittable)
 				})
 			})
 		})
